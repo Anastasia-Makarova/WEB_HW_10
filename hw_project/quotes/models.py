9acf853187937cf2346688e58396f664665bf38d
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-07 10:16:43 +0100

    migr

--- hw_project/quotes/models.py
+++ hw_project/quotes/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    ForeignKey,
+    CASCADE,
+    CharField,
+    TextField,
+    DateTimeField,
+    ManyToManyField,
+)
 
-# Create your models here.
+
+class Author(Model):
+    fullname = CharField(max_length=50)
+    born_date = CharField(max_length=50)
+    born_location = CharField(max_length=150)
+    description = TextField()
+    created_at = DateTimeField(auto_now_add=True)
+
+
+class Tag(Model):
+    name = CharField(max_length=30, null=False, unique=True)
+
+
+class Quote(Model):
+    quote = TextField()
+    tags = ManyToManyField(Tag)
+    author = ForeignKey(Author, on_delete=CASCADE, default=None, null=True)
+    created_at = DateTimeField(auto_now_add=True)
