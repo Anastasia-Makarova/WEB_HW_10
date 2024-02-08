479537c4423f5f2f94c6493dcc98e1b31fb64d9a
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-08 09:24:48 +0100

    LOGin/out done

--- hw_project/users/views.py
+++ hw_project/users/views.py
@@ -12,10 +12,10 @@ class RegisterView(View):
     template_name = 'users/register.html'
     form_class = RegisterForm
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect(to="quotes:root")
-        return super().dispatch(request, *args, **kwargs)
+    # def dispatch(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated:
+    #         return redirect(to="quotes:root")
+    #     return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         return render(request, self.template_name, {"form": self.form_class})
